fd0aea520b5d67a563ab99455d7f7a0930bd8da2
Laiz <laiz.moura@hotmail.com>
2024-11-04 23:25:44 -0300

    Adicionando movimentação

--- code/Enemy.py
+++ code/Enemy.py
@@ -10,11 +10,23 @@ class Enemy(Entity):
         super().__init__(name, position)
         self.shot_delay = ENTITY_SHOT_DELAY[self.name]
 
+        self.vertical_speed = 2 # Velocidade vertical
+        self.direction = 1
+
     def move(self):
         self.rect.centerx -= ENTITY_SPEED[self.name]
+
+        self.rect.centery += self.vertical_speed * 2
+
+        if self.rect.bottom >= WIN_HEIGHT:
+            self.direction = -1  # Muda a direção para cima
+
+        if self.rect.top <= 0:
+            self.direction = 1
 
     def shoot(self):
         self.shot_delay -= 1
         if self.shot_delay == 0:
             self.shot_delay = ENTITY_SHOT_DELAY[self.name]
             return EnemyShot(name=f'{self.name}Shot', position=(self.rect.centerx, self.rect.centery))
+
